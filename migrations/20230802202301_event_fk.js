cb5a63ef048c4d5fadc59fab2919a781083b1af6
Charles Taylor <charlestaylor95@gmail.com>
2023-08-02 16:26:49 -0400

    migration

---
+++ migrations/20230802202301_event_fk.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema
+    .alterTable('events', function (table) {
+      table.integer('issue_id').references('issues.id')
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema
+    .alterTable('events', function (table) {
+      table.dropColumn('issue_id')
+    })
+};

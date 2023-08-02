2920c3ad7599e14fcda905a4ee39d9402f091a5c
Charles Taylor <charlestaylor95@gmail.com>
2023-08-02 17:38:36 -0400

    Make database name depend on config

--- test/database.ts
+++ test/database.ts
@@ -1,24 +1,26 @@
 import Knex from 'knex'
+import knexConfig from '../knexfile'
 
 function connectAsAdmin() {
   return Knex({
-    client: 'postgresql', 
-    connection: { 
+    client: 'postgresql',
+    connection: {
       host: 'localhost',
       user: 'postgres',
-    }
+    },
   })
 }
 
+const database = knexConfig.test.connection.database
+
 export async function createTestDatabase(): Promise<void> {
   const knex = connectAsAdmin()
-  await knex.raw('CREATE DATABASE octo_events_test').catch(console.error);
+  await knex.raw('CREATE DATABASE ??', database).catch(console.error)
   knex.destroy()
-};
+}
 
 export async function dropTestDatabase(): Promise<void> {
   const knex = connectAsAdmin()
-  await knex.raw('DROP DATABASE IF EXISTs octo_events_test').catch(console.error)
-  knex.destroy();
-};
-
+  await knex.raw('DROP DATABASE IF EXISTS ??', database).catch(console.error)
+  knex.destroy()
+}

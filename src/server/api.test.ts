e40798dc228ebe0dc6549a1b164cec0fe7400a90
Charles Taylor <charlestaylor95@gmail.com>
2023-08-02 20:21:12 -0400

    Works

--- src/server/api.test.ts
+++ src/server/api.test.ts
@@ -13,14 +13,10 @@ afterAll(async () => {
   await knex.destroy()
 })
 
-beforeEach(async () => {
-  await knex.raw('START TRANSACTION')
-})
-/*
 afterEach(async () => {
-  await knex.raw("ROLLBACK")
+  await knex.raw('DELETE FROM events')
+  await knex.raw('DELETE FROM issues')
 })
-*/
 
 describe('api', () => {
   test('/hello-world', async () => {
@@ -41,29 +37,33 @@ describe('api', () => {
     })
 
     test('when the issue has events, responds with 200 and them', async () => {
-      knex.batchInsert('issues', [{ id: 34 }, { id: 42 }, { id: 57 }])
-      knex.batchInsert('events', [
-        { issue_id: 34, action: 'created' },
-        { issue_id: 34, action: 'deleted' },
-        { issue_id: 57, action: 'commented' },
-      ])
+      //await knex.transaction(async (knex) => {
+        await knex.batchInsert('issues', [{ id: 34 }, { id: 42 }, { id: 57 }])
+        await knex.batchInsert('events', [
+          { issue_id: 34, action: 'created' },
+          { issue_id: 34, action: 'deleted' },
+          { issue_id: 57, action: 'commented' },
+        ])
 
-      const response = await request(api)
-        .get('/issues/34/events')
-        .expect('Content-Type', /application\/json/)
-        .expect(200)
-      expect(response.body).toEqual([
-        { action: 'created', created_at: null },
-        { action: 'deleted', created_at: null },
-      ])
+        const response = await request(api)
+          .get('/issues/34/events')
+          .expect('Content-Type', /application\/json/)
+          .expect(200)
+        expect(response.body).toEqual([
+          { action: 'created', created_at: null },
+          { action: 'deleted', created_at: null },
+        ])
+      //})
     })
     test('when the issue has no events, responds with 200 and an empty list', async () => {
-      knex.batchInsert('issues', [{ id: 34 }, { id: 42 }, { id: 57 }])
-      const response = await request(api)
-        .get('/issues/34/events')
-        .expect('Content-Type', /application\/json/)
-        .expect(200)
-      expect(response.body).toEqual([])
+      //await knex.transaction(async (knex) => {
+        await knex.batchInsert('issues', [{ id: 34 }, { id: 42 }, { id: 57 }])
+        const response = await request(api)
+          .get('/issues/34/events')
+          .expect('Content-Type', /application\/json/)
+          .expect(200)
+        expect(response.body).toEqual([])
+      //})
     })
   })
 })

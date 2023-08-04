8821221412436beddceaa94602c330dc251182b0
Charles Taylor <charlestaylor95@gmail.com>
2023-08-04 14:43:23 -0400

    Use immediate deployment strategy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import api from '@/api'
 
-const port = 8080 //process.env.PORT
-console.log(`port: ${port}`)
+const port = process.env.PORT
 api.listen(port, () => console.log(`Octo Events API running on port ${port}`))

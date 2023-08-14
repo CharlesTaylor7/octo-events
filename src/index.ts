d01c7e0c19e1e61b67b0e6c49c9bd94c37f99785
Charles Taylor <charlestaylor95@gmail.com>
2023-08-13 21:37:44 -0400

    Set host as '0.0.0.0'

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import api from '@/api';
 
 const port: number = Number(process.env.PORT);
 
-api.listen({ port }, function (error, address) {
+api.listen({ port, host: '0.0.0.0' }, function (error, address) {
   if (error) {
     api.log.error(error);
     return;
